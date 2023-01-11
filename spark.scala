5c71d61ba1d374c52921bb1773f2a384119f7f52
queukat <75810528+queukat@users.noreply.github.com>
2023-01-11 18:33:22 +0300

    Update spark.scala

--- spark.scala
+++ spark.scala
@@ -5,40 +5,37 @@ import org.apache.spark.sql.functions._
 object OracleToHive {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().appName("OracleToHive").getOrCreate()
-    val oracleTableName = args(0)
-    val hiveTableName = args(1)
-    val oracleUrl = args(2)
-    val hiveUrl = args(3)
-    val oracleUser = args(4)
-    val oraclePassword = args(5)
-    val hiveUser = args(6)
-    val hivePassword = args(7)
-    
-    // Get Oracle Table Columns and Data Types
-    val oracleTableSchema = spark.read.format("jdbc").option("url", oracleUrl).option("dbtable", oracleTableName).option("user", oracleUser).option("password", oraclePassword).option("driver", "oracle.jdbc.driver.OracleDriver").load().schema
-    
-    // Cast Oracle Column Data Types
+    val jdbcUrl = "jdbc:oracle:thin:@hostname:port/service_name"
+    val jdbcTable = "oracle_table"
+    val jdbcUser = "username"
+    val jdbcPassword = "password"
+    val hiveTable = "hive_table"
+    val hiveDatabase = "hive_database"
+    val hiveFormat = "orc"
+    val hivePartitionColumns = Seq("column1", "column2")
+    val hivePartitionValues = Seq("value1", "value2")
+    val hivePartitionLocation = "hdfs:///hive/warehouse/hive_database.db/hive_table"
+    val streamingInterval = 10 //seconds
+
+    //Get Oracle Table Schema
+    val oracleTableSchema = spark.read.format("jdbc").option("url", jdbcUrl).option("dbtable", jdbcTable).option("user", jdbcUser).option("password", jdbcPassword).option("driver", "oracle.jdbc.driver.OracleDriver").load().schema
+
+    //Cast Oracle Column Data Types
     val castedSchema = oracleTableSchema.map(field => {
-      if (field.dataType == IntegerType) {
-        StructField(field.name, DecimalType(38, 0), field.nullable, field.metadata)
-      } else if (field.dataType == StringType) {
-        StructField(field.name, StringType, field.nullable, field.metadata)
-      } else if (field.dataType == DateType) {
-        StructField(field.name, TimestampType, field.nullable, field.metadata)
-      } else {
-        field
+      field.dataType match {
+        case StringType => StructField(field.name, StringType, field.nullable, field.metadata)
+        case IntegerType => StructField(field.name, DecimalType(38, 0), field.nullable, field.metadata)
+        case DateType => StructField(field.name, TimestampType, field.nullable, field.metadata)
+        case _ => field
       }
     })
-    
-    // Download Table in Hive with ORC Format
-    val oracleTableDF = spark.read.format("jdbc").option("url", oracleUrl).option("dbtable", oracleTableName).option("user", oracleUser).option("password", oraclePassword).option("driver", "oracle.jdbc.driver.OracleDriver").load()
-    oracleTableDF.write.format("orc").mode("overwrite").option("url", hiveUrl).option("dbtable", hiveTableName).option("user", hiveUser).option("password", hivePassword).save()
-    
-    // Change Data Capture of Oracle Table using Spark Streaming
-    val streamingDF = spark.readStream.format("jdbc").option("url", oracleUrl).option("dbtable", oracleTableName).option("user", oracleUser).option("password", oraclePassword).option("driver", "oracle.jdbc.driver.OracleDriver").load()
-    val streamingQuery = streamingDF.writeStream.format("orc").option("url", hiveUrl).option("dbtable", hiveTableName).option("user", hiveUser).option("password", hivePassword).option("checkpointLocation", "/tmp/checkpoint").start()
-    
-    // Append Streaming Data to Hive Table in ORC Format
-    streamingQuery.awaitTermination()
+
+    //Download Table in Hive with ORC Format
+    spark.read.format("jdbc").option("url", jdbcUrl).option("dbtable", jdbcTable).option("user", jdbcUser).option("password", jdbcPassword).option("driver", "oracle.jdbc.driver.OracleDriver").load(castedSchema:_*).write.format(hiveFormat).mode("overwrite").partitionBy(hivePartitionColumns:_*).saveAsTable(hiveDatabase + "." + hiveTable)
+
+    //Change Data Capture of Oracle Table using Spark Streaming from JDBC
+    val streamingDF = spark.readStream.format("jdbc").option("url", jdbcUrl).option("dbtable", jdbcTable).option("user", jdbcUser).option("password", jdbcPassword).option("driver", "oracle.jdbc.driver.OracleDriver").load(castedSchema:_*)
+    val streamingQuery = streamingDF.writeStream.format(hiveFormat).option("checkpointLocation", hivePartitionLocation).outputMode("append").partitionBy(hivePartitionColumns:_*).start(hiveDatabase + "." + hiveTable)
+    streamingQuery.awaitTermination(streamingInterval * 1000)
   }
 }
